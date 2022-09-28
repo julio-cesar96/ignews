5c4dc5c66220c7f333d779e44188efbd0f7a9115
julio-cesar96 <julio_cbs96@hotmail.com>
2022-09-27 22:15:08 -0300

    add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import React from "react"
+import Head from "next/head"
+
 
 export default function Home() {
   return (
-    <h1> Olá </h1>
+    <>
+      <Head>
+        <title>Início | ig.news </title>
+      </Head>
+      <h1> Olá </h1>
+    </>
   )
 }
